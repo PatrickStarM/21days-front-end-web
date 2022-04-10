bd697aab39c60f787f49659dbf4e124f3d3a0a7c
PatrickStarM <53247178PatrickStarM@users.noreply.github.com>
2022-04-10 14:46:09 +0800

    day-08 发表页

---
+++ 社区交友/components/common/nothing.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="text-center">
+		<image src="/static/img/nothing.png"
+		 class="w-40 h-40 my-1"></image>
+		 <view class=" text text-base">
+		 	这里什么都没有...
+		 </view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
